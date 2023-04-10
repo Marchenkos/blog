2677bc11bef4127d535236b25ef3ed4a202a4670
Ksenia <ms.ksi2000@mail.ru>
2023-04-10 12:18:39 +0300

    fix routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   resources :home, only: [:index]
 
   devise_scope :user do
-    authenticated :user do
-      root to: 'home#index', as: :user_url
-    end
+    # authenticated :user do
+    #   root to: 'home#index', as: :user_url
+    # end
 
     unauthenticated :user do
       root to: 'devise/sessions#new', as: :unauthenticated_root
     end
+  end
+
+  authenticated :user do
+    root to: 'home#index', as: :user_url
   end
 end
